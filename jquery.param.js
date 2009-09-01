95789b613d116da3d61026c1fcfb6ed6a750c365
Jacques Crocker <merbjedi@gmail.com>
2009-08-31 21:25:46 -0700

    Better Param Plugin

---
+++ jquery.param.js
@@ -0,0 +1,41 @@
+jQuery.extend({
+	// Serialize an array of form elements or a set of
+	// key/values into a query string
+	param: function( a ) {
+		var s = [];
+
+		function add( key, value ){
+			s[ s.length ] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+		}
+
+		// If an array was passed in, assume that it is an array
+		// of form elements
+		if ( jQuery.isArray(a) || a.jquery ) {
+			// Serialize the form elements
+			jQuery.each( a, function() {
+				add( this.name, this.value );
+			});
+		} else {
+			// Recursively encode parameters from object, 
+			// building a prefix path as we go down
+			function buildParams(obj, prefix)
+			{
+				if ( jQuery.isArray(obj) ) {
+					for ( var i = 0, length = obj.length; i < length; i++ ) {
+						buildParams( obj[i], prefix );
+					};
+				} else if( typeof(obj) == "object" ) {
+					for ( var j in obj ) {
+						buildParams(obj[j], (prefix ? (prefix+"["+j+"]") : j) );
+					}
+				} else {
+					add( prefix, jQuery.isFunction(obj) ? obj() : obj );
+				}
+			}
+			buildParams(a);
+		}
+
+		// Return the resulting serialization
+		return s.join("&").replace(/%20/g, "+");
+	}
+});
